0ed66d74e1acebde5151f2caff98dac2cf0b1778
Jan Vrba <janvrba.jv@gmail.com>
2017-09-16 13:33:09 +0200

    Commented GameStateReversi.

--- MCTS_Mod/GameStateReversi.cs
+++ MCTS_Mod/GameStateReversi.cs
@@ -8,10 +8,23 @@ namespace MCTS_Mod
 {
     class GameStateReversi : GameState
     {
+        /// <summary>
+        /// Tag of game state, used for miscellaneous purposes (such as determining terminal state)
+        /// </summary>
         public int tag = -1;
 
+        /// <summary>
+        /// List of valid moves.
+        /// </summary>
         private List<GameState> validMoves;
 
+        /// <summary>
+        ///  Represent the game state for the game Reversi.
+        /// </summary>
+        /// <param name="_parent">Parent of state. Null parent implies root.</param>
+        /// <param name="_playedBy">Who played this move.</param>
+        /// <param name="state">Board of this move.</param>
+        /// <param name="_LMID">Represents misc. information about previous move, such as whether it was a pass.</param>
         public GameStateReversi(GameState _parent, byte _playedBy, object state, int _LMID)
         {
             Parent = _parent;
@@ -24,24 +37,36 @@ namespace MCTS_Mod
                 this.Depth = Parent.Depth + 1;
         }
 
+        /// <summary>
+        /// Returns list of valid moves.
+        /// </summary>
+        /// <returns>List of valid moves.</returns>
         public override List<GameState> ValidMoves()
         {
             return validMoves;
         }
 
-
+        /// <summary>
+        /// Sets valid moves to "_validMoves".
+        /// </summary>
+        /// <param name="_validMoves">List of new valid moves.</param>
         public override void SetValidMoves(List<GameState> _validMoves)
         {
             validMoves = _validMoves;
         }
 
-
+        /// <summary>
+        /// Adds "val" to value of this node.
+        /// </summary>
+        /// <param name="val"></param>
         public override void AddValue(double val)
         {
             this.Value += val;
         }
 
-
+        /// <summary>
+        /// Sets parent node to null.
+        /// </summary>
         public override void RemoveParent()
         {
             this.Parent = null;
